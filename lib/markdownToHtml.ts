c6de708231bcd5ee9c323c81d69fae64c36d543b
Prajwal P <prajwalprakash3722@gmail.com>
2022-07-06 15:20:52 +0530

    [updates] redefining

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,14 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+// import { remark } from 'remark'
+// import html from 'remark-html'
+
+// export default async function markdownToHtml(markdown: string) {
+//   const result = await remark().use(html).process(markdown)
+//   return result.toString()
+// }
+
+import showdown from 'showdown'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const converter = new showdown.Converter()
+  return converter.makeHtml(markdown)
 }
