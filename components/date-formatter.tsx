4c3e26ac338c3aedc6de3d881635813ab828fd2a
Prajwal P <prajwalprakash3722@gmail.com>
2022-09-26 08:51:06 +0530

    chore:date formatting

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -4,11 +4,30 @@ type Props = {
   dateString: string;
 };
 
+const diffDays = (date_1: Date, date_2: Date) => {
+  let difference = date_1.getTime() - date_2.getTime();
+  let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+  return TotalDays;
+};
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString);
   return (
     <time dateTime={dateString} className="text-slate-100 font-medium">
-      {format(date, "LLLL	d, yyyy")}
+      {days[new Date(date).getDay()]}, {format(date, "LLLL	d, yyyy")} -{" "}
+      {diffDays(new Date(), date) !== 0
+        ? `(${diffDays(new Date(), date)} days ago)`
+        : "Today"}
     </time>
   );
 };
