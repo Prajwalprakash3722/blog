c233e6154c178390079996df404666958cb8e70b
Prajwal P <prajwalprakash3722@gmail.com>
2023-04-01 21:56:03 +0530

    chore: revert strava changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,52 +10,10 @@ import Post from "../types/post";
 import generateRssFeed from "../lib/generateRssFeed";
 import { getAllPosts } from "../lib/api";
 import AskEmail from "../components/AskEmail";
-import StravaEmbed from "../components/StravaEmbed";
-import { getActivities } from "../lib/strava";
 
 type Props = {
   allPosts: Post[];
-  activities: any;
 };
-
-const dummyActivities = [
-  {
-    id: 1,
-    activityType: "Run",
-    name: "Morning Run",
-    distance: 10000,
-    movingTime: 3600,
-    startDate: "2022-03-25T12:30:00Z",
-    averageSpeed: 2.78,
-  },
-  {
-    id: 2,
-    activityType: "Ride",
-    name: "Morning Ride",
-    distance: 72000,
-    movingTime: 14400,
-    startDate: "2022-03-24T14:00:00Z",
-    averageSpeed: 2.78,
-  },
-  {
-    id: 3,
-    activityType: "Swim",
-    name: "Morning Swim",
-    distance: 1500,
-    movingTime: 1800,
-    startDate: "2022-03-23T08:00:00Z",
-    averageSpeed: 0.83,
-  },
-  {
-    id: 1,
-    activityType: "Run",
-    name: "Morning Run",
-    distance: 10000,
-    movingTime: 3600,
-    startDate: "2022-03-25T12:30:00Z",
-    averageSpeed: 2.78,
-  },
-];
 
 const Index = ({ allPosts }: Props) => {
   const heroPost = allPosts[0];
@@ -75,7 +33,6 @@ const Index = ({ allPosts }: Props) => {
           {allPosts.length > 0 && (
             <MorePosts posts={allPosts} header="All Posts" />
           )}
-          <StravaEmbed activities={dummyActivities} />
           <AskEmail />
           <Catagories posts={allPosts} />
         </Container>
@@ -97,11 +54,9 @@ export const getStaticProps = async () => {
   ]);
   await generateRssFeed(); // calling to generate the feed
 
-  const activities = await getActivities();
   return {
     props: {
       allPosts,
-      activities,
     },
     revalidate: 3600,
   };
