5457823f7e26fefeab76cbb4be1dacf45116f8cd
Prajwal P <prajwalprakash3722@gmail.com>
2023-08-14 12:00:40 +0530

    removed preact integration, it just breaks

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
 /**@type{import('next).NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  output: "standalone",
-
-  //    used preReact to reduce the bundle size and faster response rate.
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
+  output: "standalone"
 };
