b539d33023182624891b22906b2ad76815b30738
Prajwalprakash3722 <prajwalprakash3722@gmail.com>
2026-05-23 13:21:30 +0530

    fix vulun + fix rsc

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,14 +1,7 @@
-// import { remark } from 'remark'
-// import html from 'remark-html'
-
-// export default async function markdownToHtml(markdown: string) {
-//   const result = await remark().use(html).process(markdown)
-//   return result.toString()
-// }
-
-import showdown from "showdown";
+import { remark } from "remark";
+import html from "remark-html";
 
 export default async function markdownToHtml(markdown: string) {
-  const converter = new showdown.Converter();
-  return converter.makeHtml(markdown);
+  const result = await remark().use(html).process(markdown);
+  return result.toString();
 }
